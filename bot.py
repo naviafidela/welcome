1a45ff7c8be6ed13f60dd01b73c869b5cc9d3426
naviafidela <naviafidela@gmail.com>
2024-12-21 22:46:24 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,24 +9,28 @@ bot_token = '7559677848:AAEZUWxYlCEXZgcllUDKFztK7TmS2tGs_0o'  # Ganti dengan tok
 
 app = Client("welcome_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
-# Handler untuk /start
-@app.on_message(filters.command("start"))
-async def start(client, message):
-    await message.reply_text("Website Saat Ini : https://bokep2025.us")
-
-# Handler untuk anggota baru
 @app.on_message(filters.new_chat_members)
 async def add_group(client, message):
     for member in message.new_chat_members:
-        # Membuat tombol dengan link yang diberikan
+        # Membuat tombol dengan link untuk berbagi pesan
+        share_message = (
+            "Asupan SMA 💦 : https://t.me/joinchat/7P2DFzD_s5I1MTM1\n\n"
+            "Pemersatu Bangsa 💦 : https://t.me/joinchat/vG-iFZLTulg2Zjhl"
+        )
+
+        # Encode message untuk URL
+        from urllib.parse import quote
+        encoded_message = quote(share_message)
+
+        # Membuat tombol dengan URL untuk berbagi
         keyboard = [
-            [InlineKeyboardButton("🔐Buka Kunci Media🔐", url="https://t.me/share/url?text=Asupan+SMA+💦+:+https://t.me/joinchat/7P2DFzD_s5I1MTM1+\n\n+Pemersatu+Bangsa+💦+:+https://t.me/joinchat/vG-iFZLTulg2Zjhl")]
+            [InlineKeyboardButton("🔐 Buka Kunci Media 🔐", url=f"tg://msg?text={encoded_message}")]
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
 
         # Mengirim gambar dengan spoiler dan pesan
         message = await message.reply_photo(
-            photo="https://i.ibb.co.com/L8YvcTB/6276011250815189839-120.jpg",  # Ganti dengan URL gambar yang sesuai
+            photo="https://i.ibb.co/L8YvcTB/6276011250815189839-120.jpg",  # Ganti dengan URL gambar yang sesuai
             caption=f"👋 Hai {member.full_name}\n\n"
                     "Semua Chat Disembunyikan Untuk Anggota Baru\n"
                     "Anda Harus Membuka Kunci Dengan Cara Bagikan Ke 3 - 5 Grup.\n\n"

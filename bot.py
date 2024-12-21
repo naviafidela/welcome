192155009324c6c50d75243d8befc31a8da239d7
naviafidela <naviafidela@gmail.com>
2024-12-21 22:12:53 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,10 +6,10 @@ from telegram.ext import Application, CommandHandler, MessageHandler, CallbackCo
 TOKEN = "7559677848:AAEZUWxYlCEXZgcllUDKFztK7TmS2tGs_0o"
 
 async def start(update: Update, context: CallbackContext):
-    await update.message.reply_text(f'''<b>Situs Sekarang :</b> https://bokep2025.us ''')
+    await update.message.reply_text('''𝙷𝚊𝚒 , \n\n𝙸𝚊𝚖 𝚊 𝚂𝚒𝚖𝚙𝚕𝚎 𝚆𝚎𝚕𝚌𝚘𝚖𝚎 𝙱𝚘𝚝. 𝙰𝚍𝚍 𝚖𝚎 𝚝𝚘 𝚢𝚘𝚞𝚛 𝚐𝚛𝚘𝚞𝚙 𝚊𝚗𝚍 𝚖𝚊𝚔𝚎 𝚖𝚎 𝚊𝚜 𝚊𝚍𝚖𝚒𝚗\n\n👲 𝙼𝚊𝚒𝚗𝚝𝚊𝚒𝚗𝚎𝚍 𝙱𝚢 : @BX_Botz ''')
 
 async def help(update: Update, context: CallbackContext):
-    await update.message.reply_text("➠ 𝙰𝚍𝚍 𝙼𝚎 𝚃𝚘 𝙶𝚛𝚘𝚞𝚙\n\n➠ 𝙼𝚊𝚔𝚎 𝙰𝚍𝚖𝚒𝚗 𝙼𝚎\n\n👲")
+    await update.message.reply_text("➠ 𝙰𝚍𝚍 𝙼𝚎 𝚃𝚘 𝙶𝚛𝚘𝚞𝚙\n\n➠ 𝙼𝚊𝚔𝚎 𝙰𝚍𝚖𝚒𝚗 𝙼𝚎\n\n👲 𝙼𝚊𝚒𝚗𝚝𝚊𝚒𝚗𝚎𝚍 𝙱𝚢 : @BX_Botz")
 
 async def add_group(update: Update, context: CallbackContext):
     for member in update.message.new_chat_members:
@@ -19,9 +19,10 @@ async def add_group(update: Update, context: CallbackContext):
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
 
-        # Mengirim gambar dengan spoiler dan pesan
-        message = await update.message.reply_photo(
-            photo="https://i.ibb.co.com/L8YvcTB/6276011250815189839-120.jpg",  # URL gambar
+        # Mengirim gambar sebagai spoiler
+        message = await update.message.bot.send_photo(
+            chat_id=update.message.chat_id,
+            photo="https://i.ibb.co/com/L8YvcTB/6276011250815189839-120.jpg",  # URL gambar
             caption=f"Hai {member.full_name}\n\n"
                     "Semua Chat Disembunyikan Untuk Anggota Baru\n"
                     "Anda Harus Membuka Kunci Dengan Cara Bagikan Ke 3 - 5 Grup.\n\n"
@@ -36,11 +37,12 @@ async def add_group(update: Update, context: CallbackContext):
         )
 
         # Menjadwalkan penghapusan pesan setelah 15 detik
-        await context.job_queue.run_once(delete_message, 15, context=message.chat.id)
+        await context.job_queue.run_once(delete_message, 15, context=message)
 
 async def delete_message(context: CallbackContext):
     # Menghapus pesan setelah 15 detik
-    await context.bot.delete_message(chat_id=context.job.context, message_id=context.job.context.message_id)
+    message = context.job.context
+    await context.bot.delete_message(chat_id=message.chat_id, message_id=message.message_id)
 
 # Inisialisasi Application dan bot
 application = Application.builder().token(TOKEN).build()

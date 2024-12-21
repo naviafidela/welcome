7c7fc007bf26d51397e85db44263741dab280876
naviafidela <naviafidela@gmail.com>
2024-12-21 21:41:28 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,30 @@
-from telegram import Update
-from telegram.ext import Updater , CommandHandler, CallbackQueryHandler, CallbackContext,Filters,MessageHandler
 import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, CallbackContext, filters
 
-Token =os.environ.get("7508753099:AAEDEAogPWH2Z13TmfJn0efWKImPLTI-7h8",None)
-updater = Updater( Token ,use_context = True )
+# Ganti dengan token yang sesuai jika Anda tidak menggunakan variabel lingkungan
+TOKEN = "7508753099:AAEDEAogPWH2Z13TmfJn0efWKImPLTI-7h8"
 
-def start(updater,context):
- updater.message.reply_text('''𝙷𝚊𝚒 , \n\n𝙸𝚊𝚖  𝚊  𝚂𝚒𝚖𝚙𝚕𝚎  𝚆𝚎𝚕𝚌𝚘𝚖𝚎  𝙱𝚘𝚝.  𝙰𝚍𝚍  𝚖𝚎  𝚝𝚘  𝚢𝚘𝚞𝚛  𝚐𝚛𝚘𝚞𝚙  𝚊𝚗𝚍  𝚖𝚊𝚔𝚎  𝚖𝚎  𝚊𝚜  𝚊𝚍𝚖𝚒𝚗\n\n👲 𝙼𝚊𝚒𝚗𝚝𝚊𝚒𝚗𝚎𝚍  𝙱𝚢 : @BX_Botz ''')
+async def start(update: Update, context: CallbackContext):
+    await update.message.reply_text('''𝙷𝚊𝚒 , \n\n𝙸𝚊𝚖 𝚊 𝚂𝚒𝚖𝚙𝚕𝚎 𝚆𝚎𝚕𝚌𝚘𝚖𝚎 𝙱𝚘𝚝. 𝙰𝚍𝚍 𝚖𝚎 𝚝𝚘 𝚢𝚘𝚞𝚛 𝚐𝚛𝚘𝚞𝚙 𝚊𝚗𝚍 𝚖𝚊𝚔𝚎 𝚖𝚎 𝚊𝚜 𝚊𝚍𝚖𝚒𝚗\n\n👲 𝙼𝚊𝚒𝚗𝚝𝚊𝚒𝚗𝚎𝚍 𝙱𝚢 : @BX_Botz ''')
 
-def help(updater,context):
- updater.message.reply_text("➠ 𝙰𝚍𝚍  𝙼𝚎  𝚃𝚘  𝙶𝚛𝚘𝚞𝚙\n\n➠ 𝙼𝚊𝚔𝚎  𝙰𝚍𝚖𝚒𝚗  𝙼𝚎\n\n👲 𝙼𝚊𝚒𝚗𝚝𝚊𝚒𝚗𝚎𝚍  𝙱𝚢 : @BX_Botz")\
- 
+async def help(update: Update, context: CallbackContext):
+    await update.message.reply_text("➠ 𝙰𝚍𝚍 𝙼𝚎 𝚃𝚘 𝙶𝚛𝚘𝚞𝚙\n\n➠ 𝙼𝚊𝚔𝚎 𝙰𝚍𝚖𝚒𝚗 𝙼𝚎\n\n👲 𝙼𝚊𝚒𝚗𝚝𝚊𝚒𝚗𝚎𝚍 𝙱𝚢 : @BX_Botz")
 
-def add_group(update: Update, context: CallbackContext):
+async def add_group(update: Update, context: CallbackContext):
     for member in update.message.new_chat_members:
-        update.message.reply_text(f'Hai {member.full_name} , Welcome to ln Support\n\n💖Thank💖You💖For💖Joining💖')
+        await update.message.reply_text(f'Hai {member.full_name} , Welcome to ln Support\n\n💖Thank💖You💖For💖Joining💖')
 
-add_group_handle = MessageHandler(Filters.status_update.new_chat_members, add_group)
-updater.dispatcher.add_handler(add_group_handle)
+# Inisialisasi Application dan bot
+application = Application.builder().token(TOKEN).build()
 
-dp =updater.dispatcher.add_handler
-dp(CommandHandler('start',start))
-dp(CommandHandler('help',help))
+# Menambahkan handler untuk start dan help
+application.add_handler(CommandHandler('start', start))
+application.add_handler(CommandHandler('help', help))
 
-updater.start_polling()
-updater.idle()
+# Menambahkan handler untuk anggota baru
+application.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, add_group))
+
+# Mulai polling
+if __name__ == '__main__':
+    application.run_polling()

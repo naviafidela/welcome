1092a7ac687c48197d14ac2c84f23a1e852be90f
naviafidela <naviafidela@gmail.com>
2025-09-20 07:29:28 +0700

    Create message_api.py

---
+++ message_api.py
@@ -0,0 +1,4 @@
+data = [
+    {"title": "Bokep Indo Mahawiswi", "photo": "https://google.com", "url": "https://bokepsenja.com"},
+    {"title": "Bokep Indo Janda", "photo": "https://google.com", "url": "https://bokepsenja.com"}
+]

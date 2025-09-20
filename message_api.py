62535a04a9bb267b58b0a959bcddf8a242351713
naviafidela <naviafidela@gmail.com>
2025-09-20 07:35:42 +0700

    Update message_api.py

--- message_api.py
+++ message_api.py
@@ -1,4 +1,4 @@
 data = [
     {"title": "Bokep Indo Mahawiswi", "photo": "https://cdn.streamdex.net/thumbnails/A943757.jpg", "url": "https://bokepsenja.com"},
-    {"title": "Bokep Indo Janda", "photo": "https://cdn.streamdex.net/thumbnails/A943757.jpg", "url": "https://bokepsenja.com"}
+    {"title": "Bokep Indo Janda", "photo": "https://cdn.streamdex.net/thumbnails/C981943.jpg", "url": "https://bokepsenja.com"}
 ]
